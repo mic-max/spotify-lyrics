da29cc34c10fb81cf4b5c20080b67f7d35956c2d
Michael Maxwell <mike.ian.maxwell@gmail.com>
2017-12-20 20:30:47 -0500

    works with arists like "The x" and doesn't resend lyric request when pausing

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ let logS
 const cmd = 'powershell -command "Get-Process Spotify | where {$_.mainWindowTItle} | Format-List MainWindowTitle"'
 
 function handlePowershellCommand(str) {
-	let wt = str.replace(/\n|\r|MainWindowTitle : |\(.*?\)/g, '')
-	// remove The from 'The Beatles'
+	let wt = str.replace(/\n|\r|MainWindowTitle : | \(.*?\)/g, '')
+	// remove The from the start of artist names
 	let splits = wt.split(' - ')
 
 	return {
@@ -26,14 +26,15 @@ function handlePowershellCommand(str) {
 			if(err)
 				throw err
 
-			// pausing and playing should not resend the lyrics request
 			let now = handlePowershellCommand(stdout)
-			if(now.artist && last.artist !== now.artist && last.song !== now.song) {
+			// console.log('last', last)
+			// console.log('now', now)
+			if(now.artist && now.song && (last.artist !== now.artist || last.song !== now.song)) {
 				for(let i = 0; i < 10; i++)
 					console.log('\n\n\n\n\n\n\n\n\n\n')
 				console.log('Playing:', now.artist, '-', now.song)
 				console.log('---------------------------------------------------------')
-				lyrics(now.artist + '/' + now.song, (err, data) => {
+				lyrics(now.artist, now.song, (err, data) => {
 					if(err) {
 						console.log('Lyrics Unavailable :(')
 						logFile.write('Error: ')
@@ -44,7 +45,10 @@ function handlePowershellCommand(str) {
 					logFile.write(now.artist + ' - ' + now.song + '\n')
 				})
 			}
-			myLoop(now);
+			if(now.artist && now.song)
+				myLoop(now)
+			else
+				myLoop(last)
 		})
 	}, 2000)
 })('')
--- lyrics.js
+++ lyrics.js
@@ -3,9 +3,16 @@
 let cheerio	= require('cheerio')
   , request	= require('request')
 
-function lyrics(song, done) {
+function lyrics(artist, song, done) {
 
-	const lyricsLink = song.toLowerCase().replace(/[^a-z0-9/]/g, '')
+	// u2 -> u2band
+	// two-part songs with a slace to seperate them:
+		// extraordinary girl/letterbomb
+		// face to face / short circuit
+	// artist names like USS (Ubiquity Synergy Seeker)
+		// use the full name for az lyrics
+	const whole = artist.replace(/^the /ig, '') + '/' + song
+	const lyricsLink = whole.toLowerCase().replace(/[^a-z0-9/]/g, '')
 	const link = `https://www.azlyrics.com/lyrics/${lyricsLink}.html`
 
 	// TODO: check if link is a valid url before doing the request
