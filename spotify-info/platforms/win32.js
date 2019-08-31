37108e4c91a7ea795d043dde2cb05dcc4f355957
Michael Maxwell <mike.ian.maxwell@gmail.com>
2019-08-30 19:03:50 -0700

    use regex to parse window title [windows]

--- spotify-info/platforms/win32.js
+++ spotify-info/platforms/win32.js
@@ -2,12 +2,10 @@
 
 let child_proc = require('child_process')
 
-const cmd = 'powershell -command "Get-Process Spotify | where {$_.mainWindowTItle} | Format-List MainWindowTitle"'
+const cmd = 'powershell -command "gps Spotify | where {$_.mainWindowTitle} | fl MainWindowTitle"'
 
-// returns a json object
-// must have at least: artist, song
 function handlePS(str) {
-	let wt = str.replace(/\n|\r|MainWindowTitle : | \(.*?\)/g, '')
+	const wt = str.trim().match(/^MainWindowTitle : (.*)$/)[1]
 	// console.log('Window Title:', wt)
 
 	// problematic if artist name has that string in it
@@ -16,17 +14,15 @@ function handlePS(str) {
 	// i.e. removing features, or remastered suffixes, etc.
 	const seq = ' - '
 	const split = wt.indexOf(seq)
-	const result = {
+
+	return {
 		artist: wt.substring(0, split),
 		song:   wt.substring(split + seq.length)
 	}
-
-	// console.log('Song Object:', result)
-	return result
 }
 
 module.exports = function(cb) {
-  child_proc.exec(cmd, function(err, stdout) {
+  child_proc.exec(cmd, (err, stdout) => {
     if(err)
       cb(err)
 
